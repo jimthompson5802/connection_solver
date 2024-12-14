d1ba2e481f4a7ca265c02dfb961f2f8cb7c3801f
Jim Thompson <jimthompson5802@gmail.com>
2024-12-14 19:15:05 +0000

    WIP: working uppercase echo use case

--- src/web_app/entry_app.py
+++ src/web_app/entry_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
@@ -6,6 +6,14 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/setup-puzzle", methods=["POST"])
+def setup_puzzle():
+    setup_text = request.json.get("setup")
+    setup_text = setup_text.upper()
+    print(f"Received puzzle setup: {setup_text}")
+    return jsonify({"status": "success", "upperText": setup_text})
 
 
 if __name__ == "__main__":
