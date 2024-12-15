b6844b5a40a40437a366f14c938bb3cb0d4ee82f
Jim Thompson <jimthompson5802@gmail.com>
2024-12-15 02:18:35 +0000

    wip: display puzzle words both file and image at setup

--- src/web_app/entry_app.py
+++ src/web_app/entry_app.py
@@ -1,6 +1,42 @@
+import json
+import os
+import sys
+from typing import List
+
+sys.path.append("/workspaces/connection_solver/src/agent")
+
+from workflow_manager import run_workflow, create_workflow_graph
+from puzzle_solver import PuzzleState
+from tools import read_file_to_word_list, extract_words_from_image
+
 from flask import Flask, render_template, request, jsonify
 
+
 app = Flask(__name__)
+
+# get config from api_key.json and setup openai api key
+with open("/openai/api_key.json") as f:
+    config = json.load(f)
+os.environ["OPENAI_API_KEY"] = config["key"]
+
+# read in workflow instructions
+with open("src/agent/embedvec_workflow_specification.md", "r") as f:
+    workflow_instructions = f.read()
+
+workflow_graph = create_workflow_graph()
+
+
+async def webui_puzzle_setup_function(puzzle_setup_fp: str) -> List[str]:
+    # get suffix of the file path
+    suffix = puzzle_setup_fp.split(".")[-1]
+    if suffix == "txt":
+        words = read_file_to_word_list(puzzle_setup_fp)
+    elif suffix == "png":
+        words = await extract_words_from_image(puzzle_setup_fp)
+    else:
+        ValueError(f"Unsupported file type: {suffix}")
+
+    return words
 
 
 @app.route("/")
@@ -9,12 +45,12 @@ def index():
 
 
 @app.route("/setup-puzzle", methods=["POST"])
-def setup_puzzle():
-    setup_text = request.json.get("setup")
-    setup_text = setup_text.upper()
-    print(f"Received puzzle setup: {setup_text}")
-    return jsonify({"status": "success", "upperText": setup_text})
+async def setup_puzzle():
+    puzzle_setup_fp = request.json.get("setup")
+    puzzle_words = await webui_puzzle_setup_function(puzzle_setup_fp)
+    return jsonify({"status": "success", "puzzle_words": puzzle_words})
 
 
 if __name__ == "__main__":
+
     app.run(debug=True)
