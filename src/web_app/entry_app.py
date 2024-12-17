1bf52f1142714a5887db7ac1b774a0f70276eef9
Jim Thompson <jimthompson5802@gmail.com>
2024-12-17 01:56:29 +0000

    WIP: more of solver is working

--- src/web_app/entry_app.py
+++ src/web_app/entry_app.py
@@ -47,45 +47,45 @@ async def webui_puzzle_setup_function(puzzle_setup_fp: str) -> List[str]:
     return words
 
 
-async def run_webui_workflow(
-    workflow_graph,
-    initial_state: PuzzleState,
-    runtime_config: dict,
-    *,
-    puzzle_response_function: callable = None,
-) -> None:
+# async def run_webui_workflow(
+#     workflow_graph,
+#     initial_state: PuzzleState,
+#     runtime_config: dict,
+#     *,
+#     puzzle_response_function: callable = None,
+# ) -> None:
 
-    # run workflow until the next human-in-the-loop input needed
-    async for chunk in workflow_graph.astream(initial_state, runtime_config, stream_mode="values"):
-        pass
+#     # run workflow until the next human-in-the-loop input needed
+#     async for chunk in workflow_graph.astream(initial_state, runtime_config, stream_mode="values"):
+#         pass
 
-    # continue workflow until the next human-in-the-loop input required for puzzle answer
-    while chunk["tool_status"] != "puzzle_completed":
-        current_state = workflow_graph.get_state(runtime_config)
-        logger.debug(f"\nCurrent state: {current_state}")
-        logger.info(f"\nNext action: {current_state.next}")
-        if current_state.next[0] == "apply_recommendation":
-            puzzle_response = puzzle_response_function(
-                gen_words=sorted(current_state.values["recommended_words"]),
-                gen_reason=current_state.values["recommended_connection"],
-                recommender=current_state.values["current_tool"],
-            )
+#     # continue workflow until the next human-in-the-loop input required for puzzle answer
+#     while chunk["tool_status"] != "puzzle_completed":
+#         current_state = workflow_graph.get_state(runtime_config)
+#         logger.debug(f"\nCurrent state: {current_state}")
+#         logger.info(f"\nNext action: {current_state.next}")
+#         if current_state.next[0] == "apply_recommendation":
+#             puzzle_response = puzzle_response_function(
+#                 gen_words=sorted(current_state.values["recommended_words"]),
+#                 gen_reason=current_state.values["recommended_connection"],
+#                 recommender=current_state.values["current_tool"],
+#             )
 
-            workflow_graph.update_state(
-                runtime_config,
-                {
-                    "recommendation_answer_status": puzzle_response,
-                },
-            )
-        else:
-            raise RuntimeError(f"Unexpected next action: {current_state.next[0]}")
+#             workflow_graph.update_state(
+#                 runtime_config,
+#                 {
+#                     "recommendation_answer_status": puzzle_response,
+#                 },
+#             )
+#         else:
+#             raise RuntimeError(f"Unexpected next action: {current_state.next[0]}")
 
-        # run rest of workflow until the next human-in-the-loop input required for puzzle answer
-        async for chunk in workflow_graph.astream(None, runtime_config, stream_mode="values"):
-            logger.debug(f"\nstate: {workflow_graph.get_state(runtime_config)}")
-            pass
+#         # run rest of workflow until the next human-in-the-loop input required for puzzle answer
+#         async for chunk in workflow_graph.astream(None, runtime_config, stream_mode="values"):
+#             logger.debug(f"\nstate: {workflow_graph.get_state(runtime_config)}")
+#             pass
 
-    return chunk["recommendation_correct_groups"]
+#     return chunk["recommendation_correct_groups"]
 
 
 runtime_config = {
@@ -123,6 +123,7 @@ async def setup_puzzle():
             recommendation_count=0,
             llm_retry_count=0,
             invalid_connections=[],
+            words_remaining=puzzle_words,
         )
 
         print("\nGenerating vocabulary and embeddings for the words...this may take several seconds ")
@@ -172,7 +173,38 @@ async def setup_puzzle():
 
 @app.route("/update-solution", methods=["POST"])
 async def update_solution():
-    return jsonify({"status": "Correct recommendation"})
+    data = await request.json
+    color = data.get("color")
+
+    current_state = workflow_graph.get_state(runtime_config)
+    logger.debug(f"\nCurrent state: {current_state}")
+    logger.info(f"\nNext action: {current_state.next}")
+
+    if current_state.next[0] == "apply_recommendation":
+        puzzle_response = color  # Use the color as the puzzle response
+
+        workflow_graph.update_state(
+            runtime_config,
+            {
+                "recommendation_answer_status": puzzle_response,
+            },
+        )
+    else:
+        raise RuntimeError(f"Unexpected next action: {current_state.next[0]}")
+
+    # run rest of workflow untile the next human-in-the-loop input required for puzzle answer
+    async for chunk in workflow_graph.astream(None, runtime_config, stream_mode="values"):
+        logger.debug(f"\nstate: {workflow_graph.get_state(runtime_config)}")
+        pass
+
+    return jsonify(
+        {
+            "status": "Correct recommendation",
+            "words_remaining": current_state.values["words_remaining"],
+            "connection_reason": "",
+            "recommeded_words": "",
+        }
+    )
 
 
 @app.route("/generate-next", methods=["POST"])
