f97bb0a5ff551729b4ccf642b3122326a7e107b0
Jim Thompson <jimthompson5802@gmail.com>
2024-12-21 11:26:08 +0000

    code: clean-up

--- src/web_app/entry_app.py
+++ src/web_app/entry_app.py
@@ -47,47 +47,6 @@ async def webui_puzzle_setup_function(puzzle_setup_fp: str) -> List[str]:
     return words
 
 
-# async def run_webui_workflow(
-#     workflow_graph,
-#     initial_state: PuzzleState,
-#     runtime_config: dict,
-#     *,
-#     puzzle_response_function: callable = None,
-# ) -> None:
-
-#     # run workflow until the next human-in-the-loop input needed
-#     async for chunk in workflow_graph.astream(initial_state, runtime_config, stream_mode="values"):
-#         pass
-
-#     # continue workflow until the next human-in-the-loop input required for puzzle answer
-#     while chunk["tool_status"] != "puzzle_completed":
-#         current_state = workflow_graph.get_state(runtime_config)
-#         logger.debug(f"\nCurrent state: {current_state}")
-#         logger.info(f"\nNext action: {current_state.next}")
-#         if current_state.next[0] == "apply_recommendation":
-#             puzzle_response = puzzle_response_function(
-#                 gen_words=sorted(current_state.values["recommended_words"]),
-#                 gen_reason=current_state.values["recommended_connection"],
-#                 recommender=current_state.values["current_tool"],
-#             )
-
-#             workflow_graph.update_state(
-#                 runtime_config,
-#                 {
-#                     "recommendation_answer_status": puzzle_response,
-#                 },
-#             )
-#         else:
-#             raise RuntimeError(f"Unexpected next action: {current_state.next[0]}")
-
-#         # run rest of workflow until the next human-in-the-loop input required for puzzle answer
-#         async for chunk in workflow_graph.astream(None, runtime_config, stream_mode="values"):
-#             logger.debug(f"\nstate: {workflow_graph.get_state(runtime_config)}")
-#             pass
-
-#     return chunk["recommendation_correct_groups"]
-
-
 runtime_config = {
     "configurable": {
         "thread_id": str(uuid.uuid4()),
